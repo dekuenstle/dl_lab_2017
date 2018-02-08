908cde32e41430eb72e2c813255b53f16fc4bd11
David-Elias Kuenstle <kuenstld@cs.uni-freiburg.de>
2018-02-08 14:36:59 +0100

    Rescale training plot and add png

--- final_project_robotics/experiments/Plot_Script.py
+++ final_project_robotics/experiments/Plot_Script.py
@@ -11,16 +11,16 @@ def plot_mean_rwd(file_name):
 	mean_rwd_100_eps = np.convolve(rewards,np.ones(100)/100,mode='valid')
 	plt.plot(total_steps[99:], mean_rwd_100_eps)
         
-plt.rcParams['figure.figsize'] = 14,8	
+plt.rcParams['figure.figsize'] = 6.5, 5
  
 plot_mean_rwd('MountainCar-v0-dqn.monitor.csv')
 plot_mean_rwd('MountainCar-v0-dqn-noisy-net.monitor.csv')
 plot_mean_rwd('MountainCar-v0-dqn-param-noise.monitor.csv')
 
 plt.axhline(y=-110,color='r', linestyle='-')
-plt.text(25000,-109,'-110: Environment Solved')
-plt.xlabel('Number of Steps')
-plt.ylabel('Mean Reward of 100 Episodes')
+plt.text(25000,-109,'environment solved (-110)')
+plt.xlabel('Number of Steps', fontweight='bold')
+plt.ylabel('Mean Reward of 100 Episodes', fontweight='bold')
 plt.legend(['Vanilla DDQN','DDQN with Noisy Networks','DDQN with Parameter Noise'],loc=1)
-plt.savefig('test_img.png')
-plt.show()	
+plt.savefig('plot_mountaincar_train.png', dpi = 900, bbox_inches='tight')
+plt.show()
