43f156c1b4f391c957d3043fbb171d4eb700f162
Badhreesh <badhreesh2493@gmail.com>
2018-02-08 12:42:44 +0100

    Updated Plot Script file

--- final_project_robotics/experiments/Plot_Script.py
+++ final_project_robotics/experiments/Plot_Script.py
@@ -9,17 +9,18 @@ def plot_mean_rwd(file_name):
 	steps_per_eps = data['l']
 	total_steps = np.cumsum(steps_per_eps)
 	mean_rwd_100_eps = np.convolve(rewards,np.ones(100)/100,mode='valid')
-	plt.plot(mean_rwd_100_eps)
-	
-
-
-#data_dqn = genfromtxt('MountainCar-v0-dqn.monitor.csv',delimiter=',',names=True,skip_header = 1)
-#data_noisy = genfromtxt('MountainCar-v0-dqn-noisy-net.monitor.csv',delimiter=',',names=True,skip_header = 1)
-
+	plt.plot(total_steps[99:], mean_rwd_100_eps)
+        
+plt.rcParams['figure.figsize'] = 14,8	
  
 plot_mean_rwd('MountainCar-v0-dqn.monitor.csv')
 plot_mean_rwd('MountainCar-v0-dqn-noisy-net.monitor.csv')
 plot_mean_rwd('MountainCar-v0-dqn-param-noise.monitor.csv')
 
 plt.axhline(y=-110,color='r', linestyle='-')
-plt.show()	#return mean_rwd_100_eps
+plt.text(25000,-109,'-110: Environment Solved')
+plt.xlabel('Number of Steps')
+plt.ylabel('Mean Reward of 100 Episodes')
+plt.legend(['Vanilla DDQN','DDQN with Noisy Networks','DDQN with Parameter Noise'],loc=1)
+plt.savefig('test_img.png')
+plt.show()	
